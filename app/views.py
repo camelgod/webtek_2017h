54fa3f7f2da916b6cc90a14bc84a33d950e10ce8
kentare <kentaretorvik@gmail.com>
2017-11-07 14:53:28 +0100

    lagt routing til prosjekthtml-filer

--- app/views.py
+++ app/views.py
@@ -5,18 +5,47 @@ from flask import render_template, flash, redirect, session, url_for, request
 def index():
     return render_template('index.html')
 
-@app.route('/about', methods=['GET'])
+
+@app.route('/om', methods=['GET'])
 def about():
-    return render_template('about.html')
+    return render_template('about-us.html')
 
-@app.route('/order', methods=['GET'])
-def order():
-    return render_template('about.html')
-
-@app.route('/products', methods=['GET'])
-def products():
-    return render_template('about.html')
 
 @app.route('/kontakt', methods=['GET'])
 def kontakt():
     return render_template('kontakt.html')
+
+
+@app.route('/kunnskap', methods=['GET'])
+def kunnskap():
+    return render_template('knowledge-center.html')
+
+
+@app.route('/article', methods=['GET'])
+def article():
+    return render_template('article.html')
+
+
+@app.route('/nettbutikk', methods=['GET'])
+def store():
+    return render_template('store.html')
+
+
+@app.route('/category', methods=['GET'])
+def category():
+    return render_template('dog.html')
+
+
+@app.route('/product', methods=['GET'])
+def product():
+    return render_template('product.html')
+
+
+@app.route('/cart', methods=['GET'])
+def cart():
+    return render_template('cart.html')
+
+
+@app.route('/registrer', methods=['GET'])
+def registrer():
+    return render_template('registrer.html')
